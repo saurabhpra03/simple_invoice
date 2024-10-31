814706e8dd109fb456d198e47c076bae4eaf6d7f
saurabhpra03 <saurabhpra03@gmail.com>
2024-10-31 16:24:49 +0530

    Updated kotlin and ksp version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.simple.invoice"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.simple.invoice"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
